2dd0609490cf4a5d2ff007fcff6c2fc437f90d0c
ZeStug <bolognesimichael1710@gmail.com>
2024-12-11 15:35:42 +0100

    add spyke

---
+++ Source/ModuloOstacoli/ModuloOstacoliGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ModuloOstacoliGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AModuloOstacoliGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AModuloOstacoliGameMode();
+};
+
+
+
---
+++ Source/Obstacles/Private/Obstacle.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Obstacle.h"
+
+// Sets default values
+AObstacle::AObstacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	RootComponent = CapsuleComponent;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(RootComponent);
+
+	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &AObstacle::OnOverlapBegin);
+}
+
+// Called when the game starts or when spawned
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AObstacle::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+// Called every frame
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
